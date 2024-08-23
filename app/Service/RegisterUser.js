b969e5aff82c50c15741adcbac307257b0afbf02
Varsani2520 <varsaniranjani2520@gmail.com>
2024-08-23 12:47:06 +0530

    register user addedin db

---
+++ app/Service/RegisterUser.js
@@ -0,0 +1,23 @@
+import { httpAxios } from "../httpAxios";
+
+export async function Register(formDataToSubmit) {
+  try {
+    const response = await httpAxios.post("/auth", formDataToSubmit, {
+      headers: {
+        'Content-Type': 'multipart/form-data', // Make sure the content type is set correctly for form data
+      },
+    });
+
+    if (response.status === 200) {
+      const result = response.data;
+      console.log("Registration successful:");
+      return result; // You may return the result to use it in the component if needed
+    } else {
+      console.error("Registration failed:", response.data);
+      throw new Error("Registration failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // Rethrow the error to handle it in the component
+  }
+}
