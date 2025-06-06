781801b073fb9db367185f53cdfa572377476460
v_a_r_s_a_n_i <137641079+Varsani2520@users.noreply.github.com>
2025-06-06 12:13:28 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,13 +2,10 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  const authHeader = req.headers.get('authorization');
-  const accessToken = authHeader?.replace('Bearer ', '');
-  const token = req.cookies.get('accessToken')?.value || accessToken;
+  const token = req.cookies.get('accessToken')?.value ;
   console.log('Access Token from cookies:', token);
-  console.log('Access Token in middleware:', accessToken);
 
-  if (!accessToken) {
+  if (!token) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
