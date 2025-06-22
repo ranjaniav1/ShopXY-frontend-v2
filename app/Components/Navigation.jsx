36da1c846a4fa2cc69088c58f3e430ebacb8798f
Varsani2520 <varsaniranjani@gmail.com>
2025-06-22 09:28:29 +0530

    add user context

--- app/Components/Navigation.jsx
+++ app/Components/Navigation.jsx
@@ -1,37 +1,27 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { Box, Container, useTheme } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import FullScreenNav from './Navigation/FullScreenNav';
 import SmallScreenNav from './Navigation/SmallScreenNav';
 import CustomDrawer from '../Custom/CustomDrawer';
 import NavSetting from './NavSetting';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTheme } from '../redux/reducer/ThemeReducer';
-import Cookies from 'js-cookie';
+import { useUser } from '../context/UserContext';
 
 const Navigation = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
-    const theme = useTheme();
-    const user = useSelector((state) => state?.auth?.user?.user);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const savedTheme = Cookies.get('theme'); // Read from Cookies
-        if (savedTheme) {
-            dispatch(setTheme(savedTheme));
-        }
-    }, [dispatch]);
+    const { user } = useUser()
     return (
-        <Box className="bg-body border-b border-secondary"  style={{
+        <Box className="bg-body border-b border-secondary" style={{
             position: 'fixed', // Fix the nav at the top
             top: 0, // Align to the top
             left: 0,
             right: 0,
-            zIndex: 1000,height:"80px" }}>
+            zIndex: 1000, height: "80px"
+        }}>
             <Container maxWidth="xl">
                 {/* Only visible on larger screens */}
                 <Box display={{ xs: 'none', md: 'block' }}>
-                    <FullScreenNav setDrawerOpen={setDrawerOpen} user={user}/>
+                    <FullScreenNav setDrawerOpen={setDrawerOpen} user={user} />
                 </Box>
                 {/* Only visible on smaller screens */}
                 <Box display={{ xs: 'flex', md: 'none' }}>
@@ -40,10 +30,10 @@ const Navigation = () => {
             </Container>
             <Box width={350}>
                 <CustomDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} title="Settings">
-                    <NavSetting onClose={()=>setDrawerOpen(false)}/>
+                    <NavSetting onClose={() => setDrawerOpen(false)} />
                 </CustomDrawer>
             </Box> {/* Register Modal */}
-           
+
         </Box>
     );
 };
