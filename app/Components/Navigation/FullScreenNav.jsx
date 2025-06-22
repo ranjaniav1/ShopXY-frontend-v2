43c1a10c4e3d635e0b1e53b473cb3c38ed50ff52
Varsani2520 <varsaniranjani@gmail.com>
2025-06-22 23:09:29 +0530

    try to do search model close but remain

--- app/Components/Navigation/FullScreenNav.jsx
+++ app/Components/Navigation/FullScreenNav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Box, IconButton } from "@mui/material";
 import Link from "next/link";
@@ -7,9 +7,19 @@ import NavProfileMenu from "./NavProfileMenu";
 import NavCartButton from "./NavCartButton";
 import NavAuthButtons from "./NavAuthButtons";
 import { useTheme } from "@/app/context/ThemeContext";
+import { useRouter } from "next/navigation";
 
 const FullScreenNav = ({ setDrawerOpen, user }) => {
   const { webSettings } = useTheme()
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleSearch = () => {
+    const query = searchQuery.trim();
+    if (query) {
+      router.push(`/product/${encodeURIComponent(query)}`);
+    }
+  };
   return (
     <Box
       display="flex"
@@ -19,11 +29,13 @@ const FullScreenNav = ({ setDrawerOpen, user }) => {
     >
       {/* Logo */}
       <Link href="/" className="flex item-center">
-        <img src={webSettings?.logo} alt="Site Logo" className="h-16 w-auto object-contain" /> 
+        <img src={webSettings?.logo} alt="Site Logo" className="h-16 w-auto object-contain" />
       </Link>
 
       {/* Search Field */}
-      <NavSearchBar />
+      <NavSearchBar searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        onSearch={handleSearch} />
 
       {/* Account and Cart Menu */}
       <div className="flex space-x-2">
