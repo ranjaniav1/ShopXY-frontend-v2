eb5bcb346e6b8ffec7a32dd5b0b35c2782ee1dd8
Varsani2520 <varsaniranjani@gmail.com>
2025-07-04 11:33:18 +0530

    add link tag instead of a

--- app/Common/ClientClick.jsx
+++ app/Common/ClientClick.jsx
@@ -2,6 +2,7 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const ClientLink = ({ href, className = "", children, ...rest }) => {
     const router = useRouter();
@@ -12,14 +13,14 @@ const ClientLink = ({ href, className = "", children, ...rest }) => {
     };
 
     return (
-        <a
+        <Link
             href={href}
             onClick={handleClick}
             className={`cursor-pointer ${className}`}
             {...rest}
         >
             {children}
-        </a>
+        </Link>
     );
 };
 
