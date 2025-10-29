9c391b4b8c0d155edd2d9107e56791c887d4eb13
Varsani2520 <varsaniranjani@gmail.com>
2025-10-29 15:00:41 +0530

    retry

--- app/Common/Navigation.jsx
+++ app/Common/Navigation.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import FullScreenNav from '../Components/navigation/FullScreenNav';
+import FullScreenNav from '@/app/Components/navigation/FullScreenNav';
 
 
 const Navigation = () => {
